a46a9e48df34adbae564bb1414a0edb8663e1b7a
Dev_Will <43117848+Willpine@users.noreply.github.com>
2024-09-16 18:50:06 -0300

    site

---
+++ _next/static/chunks/app/page-264f81837bcdb861.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2858:function(n,e,t){Promise.resolve().then(t.t.bind(t,8173,23)),Promise.resolve().then(t.t.bind(t,3006,23))},3006:function(){}},function(n){n.O(0,[972,173,971,23,744],function(){return n(n.s=2858)}),_N_E=n.O()}]);
