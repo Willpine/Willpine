b69b6b12039002cb4ab522e03fef71414e46d36d
Dev_Will <43117848+Willpine@users.noreply.github.com>
2024-09-17 20:16:43 -0300

    added out folder

---
+++ out/_next/static/chunks/app/blog/[id]/page-833b5ad728a78c48.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[548],{5665:function(n,e,u){Promise.resolve().then(u.t.bind(u,8259,23))},8259:function(){}},function(n){n.O(0,[963,971,23,744],function(){return n(n.s=5665)}),_N_E=n.O()}]);
